55ce1e937f16ac33685702e607fafc9df4bbd36d
cosme nascimento <cosme nascimento>
2017-04-11 21:20:10 -0300

    https

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,13 +13,24 @@ namespace comp_api
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                //.UseIISIntegration()
-                .UseUrls("http://*:8000")
-                .UseStartup<Startup>()
-                .Build();
+            var host = new WebHostBuilder() 
+                .UseKestrel(options => 
+                { 
+                    options.UseHttps("cosmecert.pem", "xmlsecretkey"); 
+                }) 
+                .UseUrls("http://*:4000", "https://*:4200") 
+                .UseContentRoot(Directory.GetCurrentDirectory()) 
+                .UseStartup<Startup>() 
+                .Build(); 
+
+            // var host = new WebHostBuilder()
+            //     .UseKestrel( options => {
+            //         options.UseHttps(null, new X509Certificate2("") )
+            //     .UseContentRoot(Directory.GetCurrentDirectory())
+            //     //.UseIISIntegration()
+            //     .UseUrls("http://*:8000")
+            //     .UseStartup<Startup>()
+            //     .Build();
 
             host.Run();
         }
